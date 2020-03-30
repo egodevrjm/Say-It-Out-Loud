93aff6d793b728ff900adcba1f94faf240b8b569
Ryan Morrison <ryanmorrison@gmail.com>
2020-03-30 22:24:10 +0100

    First commit. Alpha release

---
+++ Say It Out Loud/PhraseList.swift
@@ -0,0 +1,55 @@
+//
+//  PhraseList.swift
+//  Say It Out Loud
+//
+//  Created by Ryan Morrison on 28/03/2020.
+//  Copyright © 2020 Ryan Morrison. All rights reserved.
+//
+
+import Foundation
+
+var phraseArray = [
+"I don't feel very well",
+"My throat hurts",
+"I've lost my voice",
+"Call an ambulance",
+"I can't speak",
+"How are you feeling",
+"I don't know what I've got",
+"I feel cold",
+"I feel hot",
+"I'm hungry",
+"Sorry, I can't speak at the moment",
+"I'm self isolating",
+"I love you",
+"Change the TV channel",
+"Hey Google",
+"Alexa",
+"Hey Siri",
+"I'm bored",
+"Play some music",
+"Turn up the volume",
+"Turn down the volume",
+"Sing a song",
+"Tell me a joke",
+"Stop",
+"Next track",
+"Is it raining?"
+]
+
+
+/* HStack {
+                TextField("Add a phrase", text: $newPhrase)
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(10)
+                Button(action: {
+                    self.addPhrase()
+                }) {
+                    Image(systemName: "plus")
+                        .accentColor(.black)
+                        .padding(20)
+                }
+            }.background(Color.secondary)
+*/
---
+++ Say It Out Loud/SayItButton.swift
@@ -0,0 +1,9 @@
+//
+//  SayItButton.swift
+//  Say It Out Loud
+//
+//  Created by Ryan Morrison on 27/03/2020.
+//  Copyright © 2020 Ryan Morrison. All rights reserved.
+//
+
+import Foundation
